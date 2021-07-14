d3af33b28282ac524535f7f76fce89ecc8a4a64e
tlw8253 <87379020+tlw8253@users.noreply.github.com>
2021-07-14 14:55:27 -0600

    addin src dir

---
+++ src/main/ConverterConstants.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ConverterConstants {
+    final static boolean bTrace = false;
+    final static String sTrace = "TRACE: ";
+
+    final static double dUS_TspToTbls = 0.333333;
+    final static double dUS_TspToCups = 0.0625;
+    final static double dFeetToMeters = 0.3048;
+    final static double dMileToKilometers = 1.60934;
+
+}
---
+++ src/main/DistanceConverter.java
@@ -0,0 +1,89 @@
+package main;
+
+import java.util.Scanner;  // Import the Scanner class
+
+public class DistanceConverter {
+    final static boolean bTrace = ConverterConstants.bTrace;
+    final static String sTrace = ConverterConstants.sTrace;
+    final static String sClass = "DistanceConverter()";
+
+    Scanner objScanner = new Scanner(System.in);  // Create a Scanner object
+
+   
+    public DistanceConverter(){}
+    public void closeResources(){
+        objScanner.close();  //close the resource
+   
+    }
+
+    public void controllerDistanceConverter(int iDistanceConversionType){
+        String sMethod = " controllerDistanceConverter()";
+        //double dConversionAmount = 0.0;
+            
+        if (bTrace){
+           System.out.println(sTrace + sClass + sMethod);
+        }
+    
+        switch(iDistanceConversionType)
+        {
+            case 1:
+            convertFeetToMeters();
+            break;
+            case 2:
+              convertMilesToKilometers();
+            break;
+            default:
+              System.out.println("Invalid Distance Conversion Entered: [" + iDistanceConversionType + "]");
+            break;
+        }
+    }
+    
+    /********************************************************************************************************************
+    ** Method: convertFeetToMeters()
+    ** Google search:
+    **   1 foot = 0.3048 meters
+    ********************************************************************************************************************/
+    private void convertFeetToMeters(){
+        String sMethod = " convertFeetToMeters()";
+        double dFeet = 0.0;
+        double dMeters = 0.0;
+            
+            if (bTrace){
+               System.out.println(sTrace + sClass + sMethod);
+            }
+    
+            System.out.println("Convert Feet to Meters");
+            System.out.println("Please Enter Number of Feet to convert [1,1.25,...2,...]: ");
+            dFeet = objScanner.nextDouble();
+            dMeters = dFeet * ConverterConstants.dFeetToMeters;
+            System.out.println("[" + dFeet + "] number of feet equals: " );        
+            System.out.println("[" +  dMeters + "] meters.");              
+
+
+    }
+      
+    /********************************************************************************************************************
+    ** Method: convertMilesToKilometers()
+    ** Google search:
+    **   1 mile = 1.60934 kilometers
+    ********************************************************************************************************************/
+    private void convertMilesToKilometers(){
+        String sMethod = " convertMilesToKilometers()";
+        double dMiles = 0.0;
+        double dKilometers = 0.0;
+            
+            if (bTrace){
+               System.out.println(sTrace + sClass + sMethod);
+            }
+    
+            System.out.println("Convert Miles to Kilometers");
+            System.out.println("Please Enter Number of Miles to convert [1,1.25,...2,...]: ");
+            dMiles = objScanner.nextDouble();
+            dKilometers = dMiles * ConverterConstants.dMileToKilometers;
+            System.out.println("[" + dMiles + "] number of miles equals: " );        
+            System.out.println("[" +  dKilometers + "] kilometers.");    
+        
+    }
+
+
+}
